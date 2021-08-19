c9814d42dd95b49c67621834329f4e62d1111a8c
faro <facundo.m.rodriguez1993@gmail.com>
2021-08-18 23:40:44 -0300

    segundo-commit-modulo-operaciones

---
+++ operaciones.js
@@ -0,0 +1,20 @@
+/* Este módulo (operaciones.js)
+contiene las funciones correspondientes
+de cada tipo de operación matemática a realizar
+(+,-,* y /)*/
+
+exports.sumar = (num1, num2) => {
+  return num1 + num2;
+};
+
+exports.restar = (num1, num2) => {
+  return num1 - num2;
+};
+
+exports.multiplicar = (num1, num2) => {
+  return num1 * num2;
+};
+
+exports.dividir = (num1, num2) => {
+  return num1 / num2;
+};
