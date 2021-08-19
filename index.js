d7bb631a4429ee86b7cd885ed7d5fddda5cafc4f
faro <facundo.m.rodriguez1993@gmail.com>
2021-08-18 23:51:03 -0300

    tercer-commit-métodos-de-ejecución-final-de-proyecto

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
+/* Este módulo (index.js) recibe los comandos a ejecutar 
+de las operaciones matemáticas*/
+const operaciones = require("./operaciones");
+
+function parsearTerminos(texto) {
+  let simbolo;
+  texto.includes("+")
+    ? (simbolo = "+")
+    : texto.includes("-")
+    ? (simbolo = "-")
+    : texto.includes("*")
+    ? (simbolo = "*")
+    : texto.includes("/")
+    ? (simbolo = "/")
+    : " ";
+
+  const separador = /[+\-\*\/]/;
+  let splitText = texto.split(separador);
+  let numberText = splitText.map((item) => {
+    return Number(item);
+  });
+  return {
+    primerTermino: numberText[0],
+    segundoTermino: numberText[1],
+    operacion: simbolo,
+  };
+}
+
+function ejecutarOperacion(objOperacion) {
+  return objOperacion.operacion == "+"
+    ? operaciones.sumar(objOperacion.primerTermino, objOperacion.segundoTermino)
+    : objOperacion.operacion == "-"
+    ? operaciones.restar(
+        objOperacion.primerTermino,
+        objOperacion.segundoTermino
+      )
+    : objOperacion.operacion == "*"
+    ? operaciones.multiplicar(
+        objOperacion.primerTermino,
+        objOperacion.segundoTermino
+      )
+    : objOperacion.operacion == "/"
+    ? operaciones.dividir(
+        objOperacion.primerTermino,
+        objOperacion.segundoTermino
+      )
+    : "No hay operación para realizar";
+}
+
 function main() {
-  console.log("Hola mundo");
+  const operacionParseada = parsearTerminos(process.argv[2]);
+  const resultado = ejecutarOperacion(operacionParseada);
+  console.log(resultado);
 }
 
 main();
